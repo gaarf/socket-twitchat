1d1b37a8302c34dc1ba3de281e16ed541f23ee2e
Adrien Cahen <onlineallthetime@gmail.com>
2011-05-05 20:20:59 -0700

    env

--- etc/secrets.js
+++ etc/secrets.js
@@ -5,8 +5,8 @@ module.exports.getSecret = function(key) {
 var SECRETS = 
       { twitter:  { consumer_key: TWITTER_CONSUMER_KEY
                   , consumer_secret: TWITTER_CONSUMER_SECRET
-                  // , access_token: ''
-                  // , access_token_secret: ''
+                  , access_token: TWITTER_ACCESS_TOKEN
+                  , access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
                   }
       }
       ;
