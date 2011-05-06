0cb5b76b43099213ea09deaef34f771ad5283dc5
Adrien Cahen <onlineallthetime@gmail.com>
2011-05-05 20:54:28 -0700

    twttr

--- etc/secrets.js
+++ etc/secrets.js
@@ -2,11 +2,16 @@ module.exports.getSecret = function(key) {
   return SECRETS[key];
 };
 
+var s = process.env;
+if(NODE_ENV != 'production') {
+  s = JSON.parse(require('fs').readFileSync('./env.json'));
+}
+
 var SECRETS = 
-      { twitter:  { consumer_key: TWITTER_CONSUMER_KEY
-                  , consumer_secret: TWITTER_CONSUMER_SECRET
-                  , access_token: TWITTER_ACCESS_TOKEN
-                  , access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
+      { twitter:  { consumer_key: s['TWITTER_CONSUMER_KEY']
+                  , consumer_secret: s['TWITTER_CONSUMER_SECRET']
+                  , access_token: s['TWITTER_ACCESS_TOKEN']
+                  , access_token_secret: s['TWITTER_ACCESS_TOKEN_SECRET']
                   }
       }
       ;
